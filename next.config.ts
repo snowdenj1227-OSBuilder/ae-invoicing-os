78311747516b992048697ba0029140d19e36a815
snowdenj1227-OSBuilder <snowden.j1227@gmail.com>
2025-10-28 19:08:17 -0400

    Refactor next.config.ts to TypeScript and add config

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-module.exports = nextConfig;
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['images.unsplash.com', 'api.notion.com', 's3.amazonaws.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  experimental: {
+    optimizePackageImports: ['@heroicons/react'],
+  },
+}
+
+export default nextConfig
